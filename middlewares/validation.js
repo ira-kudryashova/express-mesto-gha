00d4e0a8c97905fef0d7776e108950825518077b
Ira Kudryashova <i.trofimova@icloud.com>
2023-07-17 23:21:00 +0300

    project 14 iteration 18

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -5,7 +5,7 @@ const urlRegex = /https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]
 
 const validateAuth = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email().pattern(urlRegex),
     password: Joi.string().required(),
   }),
 });
@@ -15,7 +15,7 @@ const validateReg = celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(urlRegex),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().email().pattern(urlRegex),
     password: Joi.string().required(),
   }),
 });
