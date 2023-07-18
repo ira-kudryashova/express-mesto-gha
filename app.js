1fc7c89a720e4b04e302583ab682d9df897fef8c
Ira Kudryashova <i.trofimova@icloud.com>
2023-07-18 23:03:07 +0300

    project 14 iteration 18

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(express.json());
 app.use(helmet());
-// app.use('/', router);
 app.use(routes);
 app.use(errors());
 app.use(error);
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,9 @@
 const error = (err, req, res, next) => {
   const { status = 500, message } = err;
-  res.status(status).send({
-    message: status === 500 ? 'Произошла ошибка на сервере' : message,
+  res.status(err.status).send({
+    message: status === 500
+      ? 'Произошла ошибка на сервере'
+      : message,
   });
   next();
 };
