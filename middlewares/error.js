f081cb276e20b102d92609a676b0efb3dd96432f
Ira Kudryashova <i.trofimova@icloud.com>
2023-07-18 23:06:06 +0300

    project 14 iteration 18

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,9 +1,7 @@
 const error = (err, req, res, next) => {
   const { status = 500, message } = err;
-  res.status(err.status).send({
-    message: status === 500
-      ? 'Произошла ошибка на сервере'
-      : message,
+  res.status(status).send({
+    message: status === 500 ? 'Произошла ошибка на сервере' : message,
   });
   next();
 };
